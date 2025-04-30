425ba4e1366436a659ac499f450a136ecc3a50ed
Alex Shchyhol <oleksandr@shchyhol.com>
2025-04-30 17:09:33 +0200

    [no-ticket] make coupler http client minimal for now

---
+++ src/lib/couplerio-client/index.ts
@@ -0,0 +1,50 @@
+import { parseTemplate } from 'url-template'
+import type { Template } from 'url-template'
+import { omit } from 'lodash'
+import { COUPLER_API_HOST } from '@/env'
+import { logger } from '@/logger'
+
+type OptionsType = {
+  request: RequestInit,
+  expand?: Parameters<Template['expand']>[0],
+}
+
+const DEFAULT_HEADERS = {
+  'Content-Type': 'application/json',
+  'Accept': 'application/json',
+}
+
+export class CouplerioClient {
+  auth: string
+  baseUrl: string
+  version: string
+
+  constructor({ auth, baseUrl, version = 'v1' }: { auth: string, baseUrl?: string, version?: string }) {
+    this.auth = auth
+    this.baseUrl = `${(COUPLER_API_HOST || baseUrl)}/${version}`
+    this.version = version
+  }
+
+  async request(pathTemplateString: string, options: OptionsType): Promise<Response> {
+    const template = parseTemplate(pathTemplateString)
+    const expand = options.expand || {}
+    const optionalHeaders = options.request?.headers || {}
+    const requestOptions = omit(options.request, 'headers')
+
+    const path = template.expand(expand)
+    const url = `${this.baseUrl}${path}`
+
+    const request = new Request(url, {
+      headers: {
+        ...optionalHeaders,
+        ...DEFAULT_HEADERS,
+        Authorization: `Bearer ${this.auth}`,
+      },
+      ...requestOptions,
+    })
+
+    logger.debug(`Request URL: ${url}`)
+
+    return await fetch(request)
+  }
+}

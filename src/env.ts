e6fa5bbf8be6ae900d28e69da4d8b02cd9624c4a
Alex Shchyhol <oleksandr@shchyhol.com>
2025-05-04 22:07:41 +0200

    Add minimal Coupler http request builder

--- src/env.ts
+++ src/env.ts
@@ -13,14 +13,6 @@ export const {
     LOG_STDIO: z.boolean().default(false),
     LOG_LEVEL: z.enum(LOG_LEVELS).default('info'),
     NODE_ENV: z.enum(ENVS).default('development'),
-<<<<<<< Updated upstream
-    COUPLER_API_HOST: z.string().url().default('https://api.coupler.io'),
-    COUPLER_ACCESS_TOKEN: z.string()
-||||||| Stash base
-    COUPLER_API_HOST: z.string().url().default('https://api.coupler.io'),
-    COUPLER_ACCESS_TOKEN: z.string().trim().min(1)
-=======
     COUPLER_API_HOST: z.string().url().default('https://app.coupler.io'),
     COUPLER_ACCESS_TOKEN: z.string().trim().min(1)
->>>>>>> Stashed changes
   })

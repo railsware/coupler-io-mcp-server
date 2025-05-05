78d3146dcafe5aa4f4dde4ca66c108104654a48e
Alex Shchyhol <oleksandr@shchyhol.com>
2025-05-05 13:01:05 +0200

    Hint the tool usage

--- src/tools/get-data/index.ts
+++ src/tools/get-data/index.ts
@@ -3,7 +3,7 @@ import { handler } from './handler'
 
 export { inputSchema } from './input-schema'
 export const name = 'get-data'
-export const description = 'Get data from a Coupler.io data flow run. Make sure to first query a sample of 5 rows from `data` table, e.g. `SELECT * from data LIMIT 5`, and then run the `get-schema` tool, to better understand the structure. The `get-schema` tool will return the JSON-encoded schema of the `data` table. When visualizing the data, do not try to reference any files, just generate a static page and use the data you get from the tools.'
+export const description = 'Get data from a Coupler.io data flow run. Make sure to first query a sample of 5 rows from `data` table, e.g. `SELECT * from data LIMIT 5`, and then run the `get-schema` tool, to better understand the structure. The `get-schema` tool will return the JSON-encoded schema of the `data` table. When visualizing the data, do not try to read any files or fetch any URLs, just generate a static page and use the data you get from the tools.'
 
 const annotations = {
   title: 'Get and query data from a Coupler.io data flow.',

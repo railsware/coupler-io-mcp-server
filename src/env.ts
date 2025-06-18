48943b1360dff93e73c799134d292ef4617f2d00
Alex Shchyhol <oleksandr@shchyhol.com>
2025-06-18 10:30:27 +0200

    [CPL-20711] add env vars for transport and auth server host

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,7 @@ import { parseEnv, z } from 'znv'
 
 const LOG_LEVELS = ['trace', 'debug', 'info', 'warn', 'error', 'fatal', 'silent'] as const
 const ENVS = ['development', 'test', 'production'] as const
+const TRANSPORTS = ['stdio', 'http'] as const
 
 export const {
   COUPLER_API_HOST,
@@ -9,12 +10,16 @@ export const {
   LOG_STDIO,
   LOG_LEVEL,
   NODE_ENV,
-  APP_VERSION
+  APP_VERSION,
+  TRANSPORT,
+  AUTHORIZATION_SERVER_HOST,
 } = parseEnv(process.env, {
     COUPLER_API_HOST: z.string().url().default('https://app.coupler.io'),
     COUPLER_ACCESS_TOKEN: z.string().trim().min(1),
     LOG_STDIO: z.boolean().default(false),
     LOG_LEVEL: z.enum(LOG_LEVELS).default('info'),
     NODE_ENV: z.enum(ENVS).default('development'),
-    APP_VERSION: z.string().trim().min(1)
+    APP_VERSION: z.string().trim().min(1),
+    TRANSPORT: z.enum(TRANSPORTS).default('stdio'),
+    AUTHORIZATION_SERVER_HOST: z.string().url().default('https://auth.coupler.io')
   })
